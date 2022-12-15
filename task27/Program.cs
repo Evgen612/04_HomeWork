26355ec789921ce635cf24e461b96b2fc3820f69
evg_smi <evg_smi@mail.ru>
2022-12-15 16:45:46 +0300

    homeork_04

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"сумма цифр в числе {A} -->{SumNums(A)}");
+
+int SumNums(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+
---
+++ task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Массив из 8 элементовю");
+
+
+Console.WriteLine($"Полученный массив: ");
+PrintArray(FillArray(8));
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0;  i < array.Length; i++)
+    {
+        array[i] = new Random(). Next(-99, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[7] + ", ");
+}
+
+
